8d9b7e0640a9408e552fc5e2f9f1ffaa045df9f3
Liu Hao <lh_mouse@126.com>
2017-12-15 09:56:11 +0800

    Update CbppResponse.

--- circe-common/src/cbpp_response.cpp
+++ circe-common/src/cbpp_response.cpp
@@ -3,12 +3,20 @@
 
 #include "precompiled.hpp"
 #include "cbpp_response.hpp"
+#include <poseidon/cbpp/message_base.hpp>
+#include <poseidon/cbpp/status_codes.hpp>
 
 namespace Circe {
 namespace Common {
 
-CbppResponse::CbppResponse(long err_code, std::string err_msg)
-	: m_err_code(err_code), m_err_msg(STD_MOVE(err_msg))
+CbppResponse::CbppResponse(long status_code)
+	: m_status_code(status_code), m_message_id(), m_payload()
+{ }
+CbppResponse::CbppResponse(boost::uint64_t message_id, Poseidon::StreamBuffer payload)
+	: m_status_code(Poseidon::Cbpp::ST_OK), m_message_id(message_id), m_payload(STD_MOVE(payload))
+{ }
+CbppResponse::CbppResponse(const Poseidon::Cbpp::MessageBase &msg)
+	: m_status_code(Poseidon::Cbpp::ST_OK), m_message_id(msg.get_id()), m_payload(msg)
 { }
 CbppResponse::~CbppResponse(){ }
 
--- circe-common/src/cbpp_response.hpp
+++ circe-common/src/cbpp_response.hpp
@@ -4,26 +4,34 @@
 #ifndef CIRCE_COMMON_CBPP_RESPONSE_HPP_
 #define CIRCE_COMMON_CBPP_RESPONSE_HPP_
 
-#include <string>
+#include <poseidon/cbpp/fwd.hpp>
+#include <poseidon/stream_buffer.hpp>
+#include <boost/cstdint.hpp>
 
 namespace Circe {
 namespace Common {
 
 class CbppResponse {
 private:
-	long m_err_code;
-	std::string m_err_msg;
+	long m_status_code;
+	boost::uint64_t m_message_id;
+	Poseidon::StreamBuffer m_payload;
 
 public:
-	CbppResponse(long err_code = 0, std::string err_msg = std::string());
+	CbppResponse(long status_code = 0);
+	CbppResponse(boost::uint64_t message_id, Poseidon::StreamBuffer payload);
+	CbppResponse(const Poseidon::Cbpp::MessageBase &msg);
 	~CbppResponse();
 
 public:
-	long get_err_code() const NOEXCEPT {
-		return m_err_code;
+	long get_status_code() const NOEXCEPT {
+		return m_status_code;
 	}
-	const std::string &get_err_msg() const NOEXCEPT {
-		return m_err_msg;
+	const Poseidon::StreamBuffer &get_payload() const NOEXCEPT {
+		return m_payload;
+	}
+	Poseidon::StreamBuffer &get_payload() NOEXCEPT {
+		return m_payload;
 	}
 };
 
