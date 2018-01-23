6640f4cab02fb3f22738b9013bc1331305c89ca3
Liu Hao <lh_mouse@126.com>
2018-01-23 17:35:38 +0800

    Update 'misc/circe.sql'.

--- misc/circe.sql
+++ misc/circe.sql
@@ -5,11 +5,11 @@ USE `circe`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `PilotCompass` (
-  `id` varchar(255) NOT NULL,
+  `key` varchar(255) NOT NULL,
   `last_access_time` datetime NOT NULL,
   `version` int(10) unsigned NOT NULL,
   `value` text NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`key`),
   KEY `last_access_time` (`last_access_time`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
