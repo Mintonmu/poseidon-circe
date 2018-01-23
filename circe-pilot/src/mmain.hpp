478fc76ccb03152afbad651f7ee0863a4c711902
Liu Hao <lh_mouse@126.com>
2018-01-23 14:00:43 +0800

    Add circe-pilot.

---
+++ circe-pilot/src/mmain.hpp
@@ -0,0 +1,17 @@
+// 这个文件是 Circe 服务器应用程序框架的一部分。
+// Copyleft 2017 - 2018, LH_Mouse. All wrongs reserved.
+
+#ifndef CIRCE_PILOT_MMAIN_HPP_
+#define CIRCE_PILOT_MMAIN_HPP_
+
+#include <poseidon/module_config.hpp>
+
+namespace Circe {
+namespace Pilot {
+
+DECLARE_MODULE_CONFIG(get_config)
+
+}
+}
+
+#endif
