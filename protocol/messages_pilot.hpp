df86d3c6d7be393d8d11238e3a50e4831d4c1bd3
Liu Hao <lh_mouse@126.com>
2018-01-23 15:59:24 +0800

    Update 'protocol/messages_pilot.hpp'.

--- protocol/messages_pilot.hpp
+++ protocol/messages_pilot.hpp
@@ -9,40 +9,65 @@
 namespace Circe {
 namespace Protocol {
 namespace Pilot {
-/*
-#define MESSAGE_NAME   HttpRequestToBox
+
+// lock_disposition
+enum {
+	LDP_LEAVE_ALONE            =  0, // Do not try acquiring a lock.
+	LDP_TRY_ACQUIRE_EXCLUSIVE  = 21, // Try acquiring the lock for exclusive (write) access. The lock is reentrant.
+	LDP_TRY_ACQUIRE_SHARED     = 22, // Try acquiring the lock for shared (read-only) access. The lock is reentrant.
+	LDP_RELEASE_EXCLUSIVE      = 28, // Negate a previous `LDP_TRY_ACQUIRE_EXCLUSIVE` operation.
+	LDP_RELEASE_SHARED         = 29, // Negate a previous `LDP_TRY_ACQUIRE_SHARED` operation.
+};
+
+// lock_state_{old,new}
+enum {
+	LST_FREE                   = 91, // The lock is not locked.
+	LST_LOCKED_EXCLUSIVE       = 92, // The lock is in exclusive mode.
+	LST_LOCKED_SHARED          = 93, // The lock is in shared mode.
+};
+
+#define MESSAGE_NAME   CompareExchangeRequest
 #define MESSAGE_ID     1101
 #define MESSAGE_FIELDS \
-	FIELD_FIXED        (client_uuid, 16)	\
-	FIELD_STRING       (client_ip)	\
-	FIELD_STRING       (auth_token)	\
-	FIELD_VUINT        (verb)	\
-	FIELD_STRING       (decoded_uri)	\
-	FIELD_ARRAY        (params,	\
-	  FIELD_STRING       (key)	\
-	  FIELD_STRING       (value)	\
+	FIELD_STRING       (key)	\
+	FIELD_ARRAY        (criteria,	\
+	  FIELD_STRING       (value_cmp)	\
+	  FIELD_STRING       (value_new)	\
 	)	\
-	FIELD_ARRAY        (headers,	\
-	  FIELD_STRING       (key)	\
-	  FIELD_STRING       (value)	\
-	)	\
-	FIELD_FLEXIBLE     (entity)	\
+	FIELD_VUINT        (lock_disposition)	\
 	//
 #include <poseidon/cbpp/message_generator.hpp>
 
-#define MESSAGE_NAME   HttpResponseFromBox
+#define MESSAGE_NAME   CompareExchangeResponse
 #define MESSAGE_ID     1102
 #define MESSAGE_FIELDS \
-	FIELD_FIXED        (box_uuid, 16)	\
-	FIELD_VUINT        (status_code)	\
-	FIELD_ARRAY        (headers,	\
-	  FIELD_STRING       (key)	\
-	  FIELD_STRING       (value)	\
-	)	\
-	FIELD_FLEXIBLE     (entity)	\
+	FIELD_VINT         (criterion_index)	\
+	FIELD_STRING       (value_old)	\
+	FIELD_VUINT        (version_new)	\
+	FIELD_VUINT        (lock_state_old)	\
+	FIELD_VUINT        (lock_state_new)	\
 	//
 #include <poseidon/cbpp/message_generator.hpp>
-*/
+
+#define MESSAGE_NAME   ExchangeRequest
+#define MESSAGE_ID     1103
+#define MESSAGE_FIELDS \
+	FIELD_STRING       (key)	\
+	FIELD_STRING       (value_new)	\
+	FIELD_VUINT        (lock_disposition)	\
+	//
+#include <poseidon/cbpp/message_generator.hpp>
+
+#define MESSAGE_NAME   ExchangeResponse
+#define MESSAGE_ID     1104
+#define MESSAGE_FIELDS \
+	FIELD_STRING       (value_old)	\
+	FIELD_VUINT        (version_new)	\
+	FIELD_VUINT        (lock_state_old)	\
+	FIELD_VUINT        (lock_state_new)	\
+	//
+#include <poseidon/cbpp/message_generator.hpp>
+
 }
 }
 }
