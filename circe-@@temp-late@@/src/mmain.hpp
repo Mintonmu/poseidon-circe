b960e01223882832bae23c57ebf34024f235ff94
Liu Hao <lh_mouse@126.com>
2018-01-23 11:20:11 +0800

    Add create_project.sh.

---
+++ circe-@@temp-late@@/src/mmain.hpp
@@ -0,0 +1,17 @@
+// 这个文件是 Circe 服务器应用程序框架的一部分。
+// Copyleft 2017 - 2018, LH_Mouse. All wrongs reserved.
+
+#ifndef CIRCE_@@TEMP_LATE@@_MMAIN_HPP_
+#define CIRCE_@@TEMP_LATE@@_MMAIN_HPP_
+
+#include <poseidon/module_config.hpp>
+
+namespace Circe {
+namespace @@Temp_late@@ {
+
+DECLARE_MODULE_CONFIG(get_config)
+
+}
+}
+
+#endif
