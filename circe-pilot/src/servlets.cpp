bcaac8e72734eefb71b9a9a8d4f1fcdb12c6c70b
Liu Hao <lh_mouse@126.com>
2018-01-31 13:17:07 +0800

    Implement `Protocol::Pilot::ExchangeRequest`.

--- circe-pilot/src/servlets.cpp
+++ circe-pilot/src/servlets.cpp
@@ -16,7 +16,7 @@ namespace Circe {
 namespace Pilot {
 
 DEFINE_SERVLET_FOR(Protocol::Pilot::ExchangeRequest, connection, req){
-	const AUTO(compass, CompassKeyRepository::open_compass(CompassKey::from_string(req.key)));
+	const AUTO(compass, CompassRepository::open_compass(CompassKey::from_string(req.key)));
 	DEBUG_THROW_ASSERT(compass);
 	LOG_CIRCE_DEBUG("Opened compass: ", compass->get_compass_key());
 
@@ -24,68 +24,64 @@ DEFINE_SERVLET_FOR(Protocol::Pilot::ExchangeRequest, connection, req){
 	int exclusive_lock_disposition;
 	switch(req.lock_disposition){
 	case Protocol::Pilot::LOCK_LEAVE_ALONE:
-		m_shared_lock_disposition    = 0;
-		m_exclusive_lock_disposition = 0;
+		shared_lock_disposition = 0;
+		exclusive_lock_disposition = 0;
 		break;
 	case Protocol::Pilot::LOCK_TRY_ACQUIRE_SHARED:
-		m_shared_lock_disposition    = 1;
-		m_exclusive_lock_disposition = 0;
+		shared_lock_disposition = 1;
+		exclusive_lock_disposition = 0;
 		break;
 	case Protocol::Pilot::LOCK_TRY_ACQUIRE_EXCLUSIVE:
-		m_shared_lock_disposition    = 0;
-		m_exclusive_lock_disposition = 1;
+		shared_lock_disposition = 0;
+		exclusive_lock_disposition = 1;
 		break;
 	case Protocol::Pilot::LOCK_RELEASE_SHARED:
-		m_shared_lock_disposition    = -1;
-		m_exclusive_lock_disposition = 0;
+		shared_lock_disposition = -1;
+		exclusive_lock_disposition = 0;
 		break;
 	case Protocol::Pilot::LOCK_RELEASE_EXCLUSIVE:
-		m_shared_lock_disposition    = 0;
-		m_exclusive_lock_disposition = -1;
+		shared_lock_disposition = 0;
+		exclusive_lock_disposition = -1;
 		break;
 	default:
-		LOG_CIRCE_ERROR("Unknown lock_disposition: ", lock_disposition);
+		LOG_CIRCE_ERROR("Unknown lock_disposition: ", req.lock_disposition);
 		DEBUG_THROW(Poseidon::Exception, Poseidon::sslit("Unknown lock_disposition"));
 	}
-	DEBUG_THROW_UNLESS(!(shared_lock_disposition < 0) || compass->is_locked_shared_by(connection->get_connection_uuid()));
-	DEBUG_THROW_UNLESS(!(exclusive_lock_disposition < 0) || compass->is_locked_exclusive_by(connection->get_connection_uuid()));
+	DEBUG_THROW_ASSERT(!(shared_lock_disposition < 0) || compass->is_locked_shared_by(connection->get_connection_uuid()));
+	DEBUG_THROW_ASSERT(!(exclusive_lock_disposition < 0) || compass->is_locked_exclusive_by(connection->get_connection_uuid()));
 
 	std::string value_old = compass->get_value();
 	bool updated = false;
+
 	// Exclusive locking is required to modify the value.
-	const bool locked = compass->try_acquire_lock_exclusive(connection);
-	if(locked){
+	const bool locked_exclusive = compass->try_lock_exclusive(connection);
+	if(locked_exclusive){
+		// This exclusive lock has to be released once.
+		--exclusive_lock_disposition;
+
 		try {
 			// Update the value safely.
 			compass->set_value(STD_MOVE(req.value_new));
 
-			// If shared locking semantics was requested, guarantee it.
-			if(shared_lock_disposition < 0){
-				
+			// If shared locking has been requested, guarantee it.
+			while(shared_lock_disposition < 0){
 				compass->release_lock_shared(connection);
-			} else if(shared_lock_disposition == 0){
-				// No-op.
-			} else {
-				// This will not fail unless we run out of memory.
-				DEBUG_THROW_ASSERT(compass->acquire_lock_shared(connection));
+				++shared_lock_disposition;
 			}
-			
+			while(shared_lock_disposition > 0){
+				DEBUG_THROW_ASSERT(compass->try_lock_shared(connection));
+				--shared_lock_disposition;
+			}
 
-
-
-
-
-		compass->set_value(STD_MOVE(req.value_new));
-		//??
-		if(exclusive_lock_disposition < 0){
-			compass->release_lock_exclusive();
-			compass->release_lock_exclusive();
-		} else if(shared_lock_disposition == 0){
-			compass->release_lock_exclusive();
-		} else {
-			// No-op
-		}
-
+			// If exclusive locking has been requested, guarantee it.
+			while(exclusive_lock_disposition < 0){
+				compass->release_lock_exclusive(connection);
+				++exclusive_lock_disposition;
+			}
+			while(exclusive_lock_disposition > 0){
+				DEBUG_THROW_ASSERT(compass->try_lock_exclusive(connection));
+				--exclusive_lock_disposition;
+			}
 		} catch(std::exception &e){
 			LOG_CIRCE_ERROR("std::exception thrown: what = ", e.what());
 			connection->shutdown(Protocol::ERR_INTERNAL_ERROR, e.what());
