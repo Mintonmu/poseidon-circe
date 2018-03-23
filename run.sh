40cea1fa64ba6142aeedd226b521da715aa0fd1a
Liu Hao <lh_mouse@126.com>
2018-03-23 10:44:34 +0800

    Update 'run.sh'.

--- run.sh
+++ run.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
-_runpath="$(find $(pwd) -path '*/lib/.libs' -type d -print0 | sed -r 's/\x00/:/g')"
-_confpath="$(pwd)/etc/circe"
+set -e
 
-if [ "$1" == "-d" ]; then
-	LD_LIBRARY_PATH="${_runpath}" ./libtool --mode=execute gdb --args poseidon "${_confpath}"
-elif [ "$1" == "-v" ]; then
-	LD_LIBRARY_PATH="${_runpath}" ./libtool --mode=execute valgrind --leak-check=full --log-file='valgrind.log' poseidon "${_confpath}"
-elif [ "$1" == "-vgdb" ]; then
-	LD_LIBRARY_PATH="${_runpath}" ./libtool --mode=execute valgrind --vgdb=yes --vgdb-error=0 --leak-check=full --log-file='valgrind.log' poseidon "${_confpath}"
-else
-	LD_LIBRARY_PATH="${_runpath}" poseidon "${_confpath}"
-fi
+_pwd="$(pwd)"
+_cmd="poseidon"
+_runpath="$(find "${_pwd}" -path '*/lib/.libs' -type d -print0 | sed -r 's/\x00/:/g')"
+_confpath="${_pwd}/etc/circe"
+
+_type="$1"
+[[ "$#" -ge 1 ]] && shift
+
+export LD_LIBRARY_PATH="${_runpath}"
+
+case "${_type}" in
+(""|"--")
+	"${_cmd}" "${_confpath}" $*
+	;;
+("-d")
+	./libtool --mode=execute gdb --args "${_cmd}" "${_confpath}" $*
+	;;
+("-v")
+	./libtool --mode=execute valgrind --leak-check=full --log-file='valgrind.log' "${_cmd}" "${_confpath}" $*
+	;;
+("-vgdb")
+	./libtool --mode=execute valgrind --vgdb=yes --vgdb-error=0 --leak-check=full --log-file='valgrind.log' "${_cmd}" "${_confpath}" $*
+	;;
+(*)
+	echo "Invalid option: ${_type}" >&2
+	exit 1
+	;;
+esac
